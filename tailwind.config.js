e98d63eb9ded0c2b9e7780cb416d520b5c282467
Manny <officialemiz@gmail.com>
2024-05-05 01:09:34 +0100

    ui: font fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,22 @@ import withMT from "@material-tailwind/react/utils/withMT";
 const config = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Sofia Pro" ],
+      serif: ["Sofia Pro" ],
+      body: ["Sofia Pro" ],
+    },
     extend: {
       fontFamily: {
         normal: ['Sofia Pro Regular'],
         italic: ['Sofia Pro Italic'],
         extralight: ['Sofia Pro ExtraLight'],
         ultralight: ['Sofia Pro UltraLight'],
-        extralightItalic: ['Sofia Pro ExtraLight Italic'],
-        ultralightItalic: ['Sofia Pro UltraLight Italic'],
         light: ['Sofia Pro Light'],
-        lightItalic: ['Sofia Pro Light Italic'],
         medium: ['Sofia Pro Medium'],
-        mediumItalic: ['Sofia Pro Medium Italic'],
         semibold: ['Sofia Pro SemiBold'],
-        semiboldItalic: ['Sofia Pro SemiBold Italic'],
         bold: ['Sofia Pro Bold'],
-        boldItalic: ['Sofia Pro Bold Italic'],
         black: ['Sofia Pro Black'],
-        blackItalic: ['Sofia Pro Black Italic'],
       },
     },
   },
